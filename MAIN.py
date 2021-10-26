737ca474623a30149231e517e795b74a40210174
julkhami <90245630+julkhami@users.noreply.github.com>
2021-10-26 17:40:48 +0200

    Create MAIN.py

---
+++ MAIN.py
@@ -0,0 +1,39 @@
+
+### Documentation-Downloader iterates through all pages of the Selenium homepage and retrieves the sought-after text content, returning it to a file.
+
+
+
+
+## GLOBAL VARIABLES
+
+
+# Selenium documentation home page
+
+url = https://www.selenium.dev/documentation/
+
+
+
+
+## IMPORT LIBRARIES
+
+
+# Uses urllib to fetch the HTML
+from urllib.request import urlopen
+
+
+# Uses Beautiful Soup to select which text elements to include.
+import bs4
+
+
+
+
+# STEP 1: Trawl through the homepage to generate the list of links FROM THE TABLE OF CONTENTS.
+
+
+  # STEP 1a: IDENTIFY THE BEAUTIFUL SOUP ELEMENT OF THESE LINKS
+  
+  
+    # STEP 1a.i.: LOOK AT THE HTML AND SEE
+
+
+
